2fdeae58b300a4c04698a2d303c89020d77b8d39
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2025-06-30 11:54:02 +0100

    refactor: modify operations to accommodate u128 binary fields (#4)

--- src/binary_field.rs
+++ src/binary_field.rs
@@ -8,7 +8,7 @@ pub trait BinaryField: Sized {
 }
 
 macro_rules! define_galois_field {
-    ($struct_name: ident, $value_type: ty, $double_type: ty, $irreducible:expr, $max_exp:expr) => {
+    ($struct_name: ident, $value_type: ty, $irreducible:expr, $max_exp:expr) => {
         #[derive(Debug, Clone, Copy)]
         pub struct $struct_name {
             pub value: $value_type,
@@ -46,38 +46,59 @@ macro_rules! define_galois_field {
                 }
             }
 
-            fn mod_irreducible(a: $double_type) -> $value_type {
+            fn mod_irreducible(a_hi: $value_type, a_lo: $value_type) -> $value_type {
                 let field_size_bits = std::mem::size_of::<$value_type>() * 8;
-                let field_size_double_bits = std::mem::size_of::<$double_type>() * 8;
 
-                let mut result = a;
-                let irreducible = $irreducible as $double_type;
+                let mut result_hi = a_hi;
+                let mut result_lo = a_lo;
+                let irreducible = $irreducible as $value_type;
 
-                for i in (field_size_bits..field_size_double_bits).rev() {
-                    if result & (1 << i) != 0 {
-                        result ^= irreducible << (i - field_size_bits);
+                for i in (field_size_bits..(2 * field_size_bits)).rev() {
+                    let bit_set = if i >= field_size_bits {
+                        result_hi & (1 << (i - field_size_bits)) != 0
+                    } else {
+                        result_lo & (1 << i) != 0
+                    };
+
+                    if bit_set {
+                        let shift_amount = i - field_size_bits;
+                        if shift_amount == 0 {
+                            result_lo ^= irreducible;
+                        } else if shift_amount < field_size_bits {
+                            let shifted_hi = irreducible >> (field_size_bits - shift_amount);
+                            let shifted_lo = irreducible << shift_amount;
+
+                            result_hi ^= shifted_hi;
+                            result_lo ^= shifted_lo;
+                        }
                     }
                 }
 
-                result as $value_type
+                result_lo
             }
 
-            fn poly_multiply_gf2(a: $value_type, b: $value_type) -> $double_type {
-                let mut result: $double_type = 0;
+            fn poly_multiply_gf2(a: $value_type, b: $value_type) -> ($value_type, $value_type) {
+                let mut result_hi: $value_type = 0;
+                let mut result_lo: $value_type = 0;
 
-                // Karatsuba multiplication algorithm,
-                // except that we use XOR instead of addition
-                let mut tempa = a as $double_type;
-                let mut tempb = b as $double_type;
+                let mut tempa_hi: $value_type = 0;
+                let mut tempa_lo: $value_type = a;
+                let mut tempb = b;
+                let bits = std::mem::size_of::<$value_type>() * 8;
+
                 while tempb != 0 {
                     if tempb & 1 != 0 {
-                        result ^= tempa;
+                        result_lo ^= tempa_lo;
+                        result_hi ^= tempa_hi;
                     }
-                    tempa <<= 1;
+
+                    tempa_hi = (tempa_hi << 1) | (tempa_lo >> (bits - 1));
+                    tempa_lo = tempa_lo << 1;
+
                     tempb >>= 1;
                 }
 
-                result
+                (result_hi, result_lo)
             }
         }
 
@@ -109,9 +130,8 @@ macro_rules! define_galois_field {
             type Output = Self;
 
             fn mul(self, other: Self) -> Self::Output {
-                let result = Self::poly_multiply_gf2(self.value, other.value);
-                let reduced = Self::mod_irreducible(result);
-
+                let (result_hi, result_lo) = Self::poly_multiply_gf2(self.value, other.value);
+                let reduced = Self::mod_irreducible(result_hi, result_lo);
                 Self { value: reduced }
             }
         }
@@ -134,12 +154,20 @@ macro_rules! define_galois_field {
     };
 }
 
-// irreducible polynomial: x^16 + x^5 + x^3 + x^2 + 1
+// irreducible polynomial: x^16 + x^5 + x^3 + x^2 + 1 (stored without x^16 term)
 // max_exp: 2^16-2
-define_galois_field!(BinaryElem16, u16, u32, 0x1002D, 65534);
-// irreducible polynomial: a^32 + a^15 + a^9 + a^7 + x^4 + x^3 + 1
+define_galois_field!(BinaryElem16, u16, 0x2D, 65534);
+// irreducible polynomial: x^32 + x^15 + x^9 + x^7 + x^4 + x^3 + 1 (stored without x^32 term)
 // max_exp: 2^32-2
-define_galois_field!(BinaryElem32, u32, u64, 0x100008299, 4294967294);
+define_galois_field!(BinaryElem32, u32, 0x8299, 4294967294);
+// irreducible polynomial: x^128 + x^7 + x^2 + x + 1 (stored without x^128 term)
+// max_exp: 2^128-2
+define_galois_field!(
+    BinaryElem128,
+    u128,
+    0x87,
+    340282366920938463463374607431768211454
+);
 
 #[cfg(test)]
 mod tests {
@@ -261,4 +289,5 @@ mod tests {
     }
     test_galois_field!(BinaryElem16, u16, binary_elem_16);
     test_galois_field!(BinaryElem32, u32, binary_elem_32);
+    test_galois_field!(BinaryElem128, u128, binary_elem_128);
 }
