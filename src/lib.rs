f6960ee5b6929d2c728303271380d670936d3db1
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2025-07-07 09:43:28 +0100

    feat: wip quadratic poly

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,10 @@ pub mod binary_fft;
 pub mod binary_field;
 pub mod merkle_tree;
 pub mod reed_solomon;
+pub mod sumcheck;
 
 pub use binary_fft::*;
 pub use binary_field::*;
 pub use merkle_tree::*;
 pub use reed_solomon::*;
+pub use sumcheck::*;
---
+++ src/sumcheck.rs
@@ -0,0 +1,57 @@
+use crate::binary_field::BinaryField;
+use std::ops::{Add, Mul};
+
+pub struct QuadraticEvals<F>
+where
+    F: Copy + BinaryField + Add<Output = F> + Mul<Output = F>,
+    F::ValueType: TryFrom<usize>,
+    <F::ValueType as TryFrom<usize>>::Error: std::fmt::Debug,
+{
+    e0: F,
+    e1: F,
+    e2: F,
+}
+
+pub struct QuadraticPoly<F>
+where
+    F: Copy + BinaryField + Add<Output = F> + Mul<Output = F>,
+    F::ValueType: TryFrom<usize>,
+    <F::ValueType as TryFrom<usize>>::Error: std::fmt::Debug,
+{
+    a: F,
+    b: F,
+    c: F,
+}
+
+impl<F> QuadraticPoly<F>
+where
+    F: Copy + BinaryField + Add<Output = F> + Mul<Output = F>,
+    F::ValueType: TryFrom<usize>,
+    <F::ValueType as TryFrom<usize>>::Error: std::fmt::Debug,
+{
+    // TODO: do we really need this function if we are not performing
+    // any operation on a,b,c and simply initializing the struct?
+    pub fn new(a: F, b: F, c: F) -> Self {
+        Self { a, b, c }
+    }
+
+    pub fn eval_quadratic(&self, r: F) -> F {
+        self.a * r * r + self.b * r + self.c
+    }
+}
+
+pub fn quadratic_from_evals<F>(at0: F, at1: F, atx: F, x: Option<F>) -> QuadraticPoly<F>
+where
+    F: Copy + BinaryField + Add<Output = F> + Mul<Output = F>,
+    F::ValueType: TryFrom<usize>,
+    <F::ValueType as TryFrom<usize>>::Error: std::fmt::Debug,
+{
+    // TODO: Can we unwrap x in a simpler way from Option<F>?
+    let x = x.unwrap_or_else(|| F::new(F::ValueType::try_from(3).unwrap()));
+    let numerator = atx + at0 + x * (at1 + at0);
+    let denominator = x * x + x;
+    // TODO: is there a better way to do this than .unwrap()?
+    let a = numerator + denominator.inverse().unwrap();
+    let b = at1 + at0 + a;
+    return QuadraticPoly { a, b, c: at0 };
+}
