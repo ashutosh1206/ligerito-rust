70feca7e7a9ecd9dc3c75f3515ffba6591b50bcb
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2025-07-29 11:33:39 +0100

    fix: appropriately convert get_field return type

--- src/emulated_fs.rs
+++ src/emulated_fs.rs
@@ -19,28 +19,42 @@ impl FS {
         self.hasher.update(elems);
     }
 
-    pub fn squeeze(&mut self) -> usize {
+    pub fn squeeze(&mut self) -> u128 {
         self.hasher.update(&self.counter.to_le_bytes());
         self.counter += 1;
         let hasher_copy = self.hasher.clone();
         let result = hasher_copy.finalize();
-        u64::from_le_bytes([
+        u128::from_le_bytes([
             result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
-        ]) as usize
+            result[8], result[9], result[10], result[11], result[12], result[13], result[14],
+            result[15],
+        ])
     }
 
     pub fn get_field<F>(&mut self) -> F
     where
         F: Copy + BinaryField + Add<Output = F> + Mul<Output = F> + PartialEq + std::fmt::Debug,
-        F::ValueType: TryFrom<usize> + Copy,
-        <F::ValueType as TryFrom<usize>>::Error: std::fmt::Debug,
+        F::ValueType: TryFrom<u128> + Copy,
+        <F::ValueType as TryFrom<u128>>::Error: std::fmt::Debug,
     {
         let value = self.squeeze();
-        F::new(F::ValueType::try_from(value).unwrap())
+        // For binary-field elements smaller than 64 bits, so we need to mask to fit
+        // This should be cryptographically-safe considering we just want randomness
+        match std::mem::size_of::<F::ValueType>() {
+            1 => F::new(F::ValueType::try_from(value as u8 as u128).unwrap()),
+            2 => F::new(F::ValueType::try_from(value as u16 as u128).unwrap()),
+            4 => F::new(F::ValueType::try_from(value as u32 as u128).unwrap()),
+            8 => F::new(F::ValueType::try_from(value as u64 as u128).unwrap()),
+            16 => F::new(F::ValueType::try_from(value).unwrap()),
+            _ => panic!(
+                "Unsupported field size {}",
+                std::mem::size_of::<F::ValueType>()
+            ),
+        }
     }
 
     pub fn get_query(&mut self, n: usize) -> usize {
-        let value = self.squeeze();
+        let value = self.squeeze() as usize;
         value % n
     }
 
