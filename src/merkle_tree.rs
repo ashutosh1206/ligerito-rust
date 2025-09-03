0bd0819218165603852055e61121e7236cf44251
Ashutosh Ahelleya <ashutosh.ahelleya@gmail.com>
2025-09-03 14:48:59 +0100

    feat: optimize allocations in hashing

--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -1,6 +1,5 @@
 use crate::binary_fft::is_power_of_2;
 use blake3;
-use sha2::Sha256;
 
 fn hash_array<T>(x: Vec<T>) -> Vec<u8>
 where
@@ -111,15 +110,15 @@ pub fn prove(tree: &[Vec<u8>], queries: &[usize]) -> Vec<Vec<u8>> {
     proof
 }
 
-fn hash_siblings(left: &[u8], right: &[u8]) -> Vec<u8> {
-    let mut combined = Vec::with_capacity(64);
-    combined.extend_from_slice(left);
-    combined.extend_from_slice(right);
-    blake3::hash(&combined).as_bytes().to_vec()
+fn hash_siblings(left: &[u8], right: &[u8]) -> [u8; 32] {
+    let mut hasher = blake3::Hasher::new();
+    hasher.update(left);
+    hasher.update(right);
+    *hasher.finalize().as_bytes()
 }
 
 fn verify_ith_layer(
-    layer: &mut Vec<Vec<u8>>,
+    layer: &mut Vec<[u8; 32]>,
     queries: &mut Vec<usize>,
     curr_cnt: usize,
     proof: &[Vec<u8>],
@@ -172,9 +171,9 @@ pub fn verify<T>(
 where
     T: AsRef<[u8]>,
 {
-    let mut layer: Vec<Vec<u8>> = leaves
+    let mut layer: Vec<[u8; 32]> = leaves
         .iter()
-        .map(|leaf| blake3::hash(leaf.as_ref()).as_bytes().to_vec())
+        .map(|leaf| *blake3::hash(leaf.as_ref()).as_bytes())
         .collect();
     let mut queries = leaf_indices.to_vec();
     let mut curr_cnt = queries.len();
